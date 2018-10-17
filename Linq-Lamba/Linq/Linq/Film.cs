b2867630b078e8a3aec9b3351d64fe77c68856e0
Vescatur <ivan@edmi.nl>
2018-10-17 14:43:35 +0200

    linq

---
+++ Linq-Lamba/Linq/Linq/Film.cs
@@ -0,0 +1,16 @@
+﻿namespace Linq
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public int ReleaseYear {get; set; }
+        public string Director { get; set; }
+
+        public Film(string name, int releaseYear, string director)
+        {
+            Name = name;
+            ReleaseYear = releaseYear;
+            Director = director;
+        }
+    }
+}
---
+++ Linq-Lamba/Linq/Linq/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<Film> films = new List<Film>
+            {
+                new Film("Spiderman", 1999, "David"),
+                new Film("Breaking bad", 2005, "Martin"),
+                new Film("Smallfoot", 1995, "Lynch"),
+                new Film("Venom", 1989, "Abbas"),
+                new Film("First Man", 1993, "Joel"),
+                new Film("Jonny English", 1997, "Steven"),
+                new Film("Big bang theorie", 1955, "Errol"),
+            };
+
+
+            var recentFilms = films.Where(i => i.ReleaseYear >= 1990).OrderByDescending(i => i.ReleaseYear).Select(i=>i.ReleaseYear);
+            foreach (var recentFilm in recentFilms)
+            {
+                Console.WriteLine($"{recentFilm}");
+            }
+            Console.WriteLine();
+            films.RemoveAt(1);
+
+            foreach (var recentFilm in recentFilms)
+            {
+                Console.WriteLine($"{recentFilm}");
+            }
+            Console.WriteLine();
+
+
+            var recentFilms2 = from f in films where f.ReleaseYear >= 1990 orderby f.ReleaseYear descending select f.ReleaseYear;
+            foreach (var recentFilm in recentFilms2)
+            {
+                Console.WriteLine($"{recentFilm}");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
